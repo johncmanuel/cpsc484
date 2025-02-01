8a8668ed3e9d38adc68ce16232647e086eb40246
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2025-02-01 10:19:26 -0800

    vector assignment

---
+++ assignments/vectors/main.cpp
@@ -0,0 +1,20 @@
+//============================================================
+// file: main.cpp
+//============================================================
+#include <iostream>
+#include <cstring>
+#include <initializer_list>
+#include <cassert>
+
+//MATRIX and VECTOR classes assignment
+#include "vector3d.h"
+#include "matrix3d.h"
+
+int main(int argc, const char * argv[]) {
+  vector3D::run_tests();
+  matrix3D::run_tests(); 
+    
+  return 0;
+}
+
+
